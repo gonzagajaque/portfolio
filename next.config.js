1e07dd92aba25e5a9e9bfc13b6291036a72da329
gonzagajaque <jaqueline_gonzaga@hotmail.com>
2025-12-01 13:03:18 -0300

    feat: configure Next.js for static export to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/portfolio',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
-
-
 
